62ff9345e9cc98ea5d760bd4efec86d08fdbcbbe
MingMing <pakyowthis@gmail.com>
2018-09-07 19:36:12 +0800

    Create ptoolsinsv2.sh

---
+++ ptoolsinsv2.sh
@@ -0,0 +1,16 @@
+apt-get update
+apt-get upgrade
+clear
+apt-get install git
+apt-get install python
+apt-get install python2
+apt-get update
+clear
+git clone https://github.com/JaysonSky/Pentesting-Tools.git
+echo "###################################################"
+echo "######## Facebook : fb.com/meowwyaww.cat.ph #######"
+echo "########## Github : github.com/JaysonSky/ #########"
+echo "##### Termux : ( Pentesting Tools Installer ) #####"
+echo "###################################################"
+echo "######### Type : python2 + (python script) ########"
+echo "###################################################"
