82126f72411797eb6207002001015ddec0480e1f
MingMing <pakyowthis@gmail.com>
2018-09-07 17:19:12 +0800

    Update metasploitinsv2.sh

--- metasploitinsv2.sh
+++ metasploitinsv2.sh
@@ -6,6 +6,9 @@ echo "Wait a Few Minutes For Installing"
 chmod +x msfinstall
 ./msfinstall
 clear
+echo "#################################################"
 echo "# Facebook facebook.com/meowwyaww.cat.ph        #" 
 echo "# Github : github.com/JaysonSky/ <<             #"
+echo "# Gnuroot Debian - Metasploit Installer         #"
 echo "# type : msfconsole or rather than ./msfconsole #"
+echo "#################################################"
