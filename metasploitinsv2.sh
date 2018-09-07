8de6a525c415a1d6e586a2ada0b26685788812db
MingMing <pakyowthis@gmail.com>
2018-09-07 18:38:16 +0800

    Update metasploitinsv2.sh

--- metasploitinsv2.sh
+++ metasploitinsv2.sh
@@ -1,5 +1,4 @@
 apt-get update
-apt-get upgrade
 apt-get install curl
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \ chmod 755 msfinstall && \ ./msfinstall
 echo "Wait a Few Minutes For Installing"
