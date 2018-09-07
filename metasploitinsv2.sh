8c74cf40e753de3267ab4fba8d60c0c3e5f8fc1a
MingMing <pakyowthis@gmail.com>
2018-09-07 16:56:07 +0800

    Create metasploitinsv2.sh

---
+++ metasploitinsv2.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get upgrade
+apt-get install curl
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \ chmod 755 msfinstall && \ ./msfinstall
+clear
+echo " \ Facebook facebook.com/meowwyaww.cat.ph/\nGithub : github.com/JaysonSky"
+echo "type : msfconsole or rather than ./msfconsole"
