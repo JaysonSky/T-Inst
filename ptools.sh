e24d4126b8549096b19a2345a9c340a60a9416e3
MingMing <pakyowthis@gmail.com>
2018-09-07 16:34:22 +0800

    Create ptools.sh

---
+++ ptools.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get upgrade
+clear
+apt-get install git
+apt-get install python
+apt-get install python2
+apt-get install figlet
+apt-get update
+clear
+git clone https://github.com/JaysonSky/Pentesting-Tools.git
+figlet ~Meoww.
+echo " Facebook : fb.com/meowwyaww.cat.ph\nGithub : https://github.com/JaysonSky/"
